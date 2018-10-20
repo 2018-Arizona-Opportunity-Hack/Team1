b650f1e50622246660a189a72c93154eaa34d33c
Ryan Yiu <ryiu012@gmail.com>
2018-10-20 15:48:47 -0700

    Storing a list of files works

--- front-end/src/FileDragAndDrop.tsx
+++ front-end/src/FileDragAndDrop.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Dropzone from 'react-dropzone';
 
 interface Props {
   returnFileList: Function;
@@ -9,58 +10,16 @@ class FileDragAndDrop extends React.Component<Props> {
     super(props);
   }
 
-  dropHandler(event: React.DragEvent) {
-    console.log('Dropped a file');
-
-    event.preventDefault();
-
-    let newFileList = [];
-
-    if (event.dataTransfer.items) {
-      let fileList = event.dataTransfer.items;
-      console.log('Shit');
-      for (let i = 0; i < fileList.length; i++) {
-        if (fileList[i].kind == 'file') {
-          let file = fileList[i].getAsFile();
-          newFileList.push(file);
-          console.log('File name: ' + file.name);
-        }
-      }
-    } else {
-      for (let i = 0; i < event.dataTransfer.files.length; i++) {
-        newFileList.push(event.dataTransfer.files[i]);
-        console.log('File name: ' + event.dataTransfer.files[i].name);
-      }
-    }
-
-    this.props.returnFileList(newFileList);
-    
-    this.removeDragData(event);
+  onDrop(files: []) {
+    console.log(files);
+    this.props.returnFileList(files);
   }
-
-  dragOverHandler(event: React.DragEvent) {
-    console.log('File(s) in drop zone');
-
-    event.preventDefault();
-  }
-
-  removeDragData(event: React.DragEvent) {
-    console.log('Removing drag data');
-
-    if (event.dataTransfer.items) {
-      event.dataTransfer.items.clear();
-    } else {
-      event.dataTransfer.clearData();
-    }
-  }
-
+  
   render() {
     return (
-      <div
-        className='drop_zone'
-        onDrop={(event: React.DragEvent) => this.dropHandler(event)}
-        onDrag={(event: React.DragEvent) => this.dragOverHandler(event)}
-      />
+      <div>
+        <Dropzone onDrop={this.onDrop.bind(this)}/>
+      </div>
     );
   }
 }
