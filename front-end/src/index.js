42eb85b4154b1a2b3919faea5137a67f29066b39
Ryan Yiu <ryiu012@gmail.com>
2018-10-20 12:28:04 -0700

    Front-end shit

---
+++ front-end/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MainApp extends React.Component {
+  render() {
+    return <div>TEST</div>;
+  }
+}
+
+ReactDOM.render(<MainApp />, document.getElementById('main-app'));
