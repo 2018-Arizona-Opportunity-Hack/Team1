5698988ec00ef6a75eb1c6d8696c39aba65f23ba
Ryan Yiu <ryiu012@gmail.com>
2018-10-21 10:14:53 -0700

    Fuck Typescript

--- front-end/src/csv-utils/csvData.model.js
+++
@@ -1,9 +0,0 @@
-export class CSVData {
-    public keyHeader : string;
-    public dataHeader : string;
-
-    public keyStrings : Array<string> = [];
-    public dataStrings : Array<string> = [];
-
-    constructor () {};
-}
