4e74801bfb574f47274e35dc4e0750c0c687964c
GeoffreyHW <wonggh97@gmail.com>
2018-10-20 22:44:08 -0700

    Graphs

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,4 +1,6 @@
 import { CSVUtils } from './csv-utils/csvParseUtils'
+import Chart from './Graphs';
+
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
@@ -11,6 +13,7 @@ import '../styles/styles.less';
 
 interface State {
     files: File[];
+    chartData: {}
 }
 
 const data = [['attribute', 'attribute2'], ['value1', 'value2']];
@@ -20,9 +23,10 @@ class MainApp extends React.Component<{}, State> {
         super(props);
         
         this.returnFileList = this.returnFileList.bind(this);
-        this.state = {
-            files: []
-        }
+        this.state = ({
+            files: [],
+            chartData: {}
+        })
     }
 
     returnFileList(fileList: File[]) {
@@ -30,6 +34,41 @@ class MainApp extends React.Component<{}, State> {
             return { files: fileList.concat(state.files) }
         });
     }
+
+    componentWillMount(){
+        this.getChartData();
+      }
+
+    getChartData(){
+        // Ajax calls here
+        this.setState({
+          chartData:{
+            labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+            datasets:[
+              {
+                label:'Population',
+                data:[
+                  617594,
+                  181045,
+                  153060,
+                  106519,
+                  105162,
+                  95072
+                ],
+                backgroundColor:[
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(255, 99, 132, 0.6)'
+                ]
+              }
+            ]
+          }
+        });
+      }
 
     render() {
         // test code remove
@@ -39,6 +78,7 @@ class MainApp extends React.Component<{}, State> {
             <div className='wrapper'>
                 <FileDragAndDrop returnFileList={(fileList: File[]) => this.returnFileList(fileList)} />
                 {/* <PivotTableContainer data={data}/> */}
+                <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
             </div>
         );
     }
