065afe03b9929217bf27e6e703c89869fcdca213
Ryan Yiu <ryiu012@gmail.com>
2018-10-20 23:05:48 -0700

    Spaces

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,7 +1,6 @@
 import { CSVUtils } from './csv-utils/csvParseUtils'
 import Chart from './Graphs';
 
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as d3 from 'd3'; 
@@ -39,7 +38,6 @@ class MainApp extends React.Component<{}, State> {
         });
     }
 
-<<<<<<< HEAD
     componentWillMount(){
         this.getChartData();
       }
@@ -74,7 +72,6 @@ class MainApp extends React.Component<{}, State> {
           }
         });
       }
-=======
     componentDidUpdate() {
         if( this.state.files.length != 0 )
         {
@@ -84,16 +81,9 @@ class MainApp extends React.Component<{}, State> {
             });
         }
     }
->>>>>>> 269c70a1d1fef25e6c4b731985972872cb6766cc
 
     render() {
         return (
-<<<<<<< HEAD
-            <div className='wrapper'>
-                <FileDragAndDrop returnFileList={(fileList: File[]) => this.returnFileList(fileList)} />
-                {/* <PivotTableContainer data={data}/> */}
-                <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
-=======
             <div>
                 <div className="dragger" >
                     <div className='wrapper'>
@@ -147,8 +137,8 @@ class MainApp extends React.Component<{}, State> {
                         </div>
                     </div>
                 </div>
->>>>>>> 269c70a1d1fef25e6c4b731985972872cb6766cc
             </div>
+            <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
         </div>
         );
     }
