370e1d9fbd56cc035588fbf946e12262a456488c
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-10-21 10:33:25 -0700

    cream pie

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -134,7 +134,7 @@ class MainApp extends React.Component {
                     <div className='card-body'>
                       <Chart
                         chartData={this.state.chartData}
-                        chartType='PIE'
+                        chartType='cream PIE'
                         location='Massachusetts'
                         legendPosition='bottom'
                       />
