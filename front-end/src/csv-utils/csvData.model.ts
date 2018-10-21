7650c3630b922ae73f6bdf9656c699cb011004ab
steven85048 <steven85048@gmail.com>
2018-10-20 23:49:16 -0700

    projection functionality

---
+++ front-end/src/csv-utils/csvData.model.ts
@@ -0,0 +1,9 @@
+export class CSVData {
+    public keyHeader : string;
+    public dataHeader : string;
+
+    public keyStrings : Array<string> = [];
+    public dataStrings : Array<string> = [];
+
+    constructor () {};
+}
